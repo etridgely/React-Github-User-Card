9a252feeed8c64b77ad7c12adf2c06079b7fdb6d
etridgely <5757423+etridgely@users.noreply.github.com>
2019-09-08 23:16:00 -0700

    first attempt

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import UserCard from './Components/UserCard';
-import {BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
+import UserCard from './components/UserCard';
 import './App.css';
 
-function App() {
-  return (
-   <Router>
-     <div className="App">
-       <UserCard/>
-     </div>
-   </Router>
-    
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      user: [],
+      followers: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/etridgely`)
+      .then(res => this.setState(res))
+  }
+  render() {
+
+    return (
+      <div className="App">
+        {console.log(this.state.followers)}
+        {<UserCard
+          key={this.state.user.id}
+          imgURL={this.state.user.avatar_url}
+          name={this.state.user.login}
+          username={this.state.user.login}
+          bio={this.state.user.bio}
+          profile={this.state.user.blog}
+        />}
+        {this.state.followers.map(user => {
+          return <UserCard
+          key={user.id}
+          imgURL={user.avatar_url}
+          username={user.login}
+          profile={user.html_url}
+        />
+        })}
+      </div>
+      );
+      }
+  }
 
 export default App;
--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Card, Icon, Image } from 'semantic-ui-react';
 
-class App extends React.Component {
+const UserCard = props => {
+    return (
+        <Card>
+        <Image src={props.imgURL} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{props.name}</Card.Header>
+          <Card.Meta>{props.username}</Card.Meta>
+          <Card.Description>{props.bio}</Card.Description>
+        </Card.Content>    
+        </Card>
+    );
+};
 
-}
+export default UserCard;
